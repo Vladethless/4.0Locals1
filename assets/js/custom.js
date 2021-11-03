278c4e7122182e72320ba935402075f2bf3ba0ea
Dyshkant Vlad <vladethless@ukr.net>
2021-11-03 15:31:21 +0200

    new

---
+++ assets/js/custom.js
@@ -0,0 +1,294 @@
+(function($) {
+
+	'use strict';
+	// Mean Menu
+	$('.mean-menu').meanmenu({
+		meanScreenWidth: "991"
+	});
+	
+	// Header Sticky, Go To Top JS
+	$(window).on('scroll', function() {
+		// Header Sticky JS
+		if ($(this).scrollTop() >150){  
+			$('.navbar-area').addClass("is-sticky");
+		}
+		else{
+			$('.navbar-area').removeClass("is-sticky");
+		};
+
+		// Go To Top JS
+		var scrolled = $(window).scrollTop();
+		if (scrolled > 300) $('.go-top').addClass('active');
+		if (scrolled < 300) $('.go-top').removeClass('active');
+	});
+
+	// Luxury Villa Slider JS
+	$('.luxury-villa-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 0,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='ri-arrow-left-line'></i>",
+			"<i class='ri-arrow-right-line'></i>",
+		],
+	});
+
+	// Client Slider Two JS
+	$('.client-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 30,
+		nav: false,
+		dots: true,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+	});
+
+	// Partner Slider JS
+	$('.partner-slider').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: false,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 2,
+			},
+			414: {
+				items: 2,
+			},
+			576: {
+				items: 3,
+			},
+			768: {
+				items: 4,
+			},
+			992: {
+				items: 5,
+			},
+			1200: {
+				items: 6,
+			},
+		},
+	});
+
+	// Developments Slider JS
+	$('.developments-slider').owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: false,
+		dots: true,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			414: {
+				items: 1,
+			},
+			576: {
+				items: 2,
+			},
+			768: {
+				items: 2,
+			},
+			992: {
+				items: 3,
+			},
+			1200: {
+				items: 4,
+			},
+		},
+	});
+
+	// Featured Slider JS
+	$('.featured-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 30,
+		nav: false,
+		dots: true,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+	});
+
+	// Property Slider JS
+	$('.property-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 30,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='ri-arrow-left-line'></i>",
+			"<i class='ri-arrow-right-line'></i>",
+		],
+	});
+	
+	// Click Event JS
+	$('.go-top').on('click', function() {
+		$("html, body").animate({ scrollTop: "0" }, 50);
+	});
+
+	// Count Time JS
+	function makeTimer() {
+		var endTime = new Date("november  30, 2021 17:00:00 PDT");			
+		var endTime = (Date.parse(endTime)) / 1000;
+		var now = new Date();
+		var now = (Date.parse(now) / 1000);
+		var timeLeft = endTime - now;
+		var days = Math.floor(timeLeft / 86400); 
+		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+		if (hours < "10") { hours = "0" + hours; }
+		if (minutes < "10") { minutes = "0" + minutes; }
+		if (seconds < "10") { seconds = "0" + seconds; }
+		$("#days").html(days + "<span>Days</span>");
+		$("#hours").html(hours + "<span>Hours</span>");
+		$("#minutes").html(minutes + "<span>Minutes</span>");
+		$("#seconds").html(seconds + "<span>Seconds</span>");
+	}
+	setInterval(function() { makeTimer(); }, 300);
+
+	// Preloader
+	$(window).on('load', function() {
+		$('.preloader').addClass('preloader-deactivate');
+	}) 
+
+	// Subscribe form JS
+	$(".newsletter-form").validator().on("submit", function (event) {
+		if (event.isDefaultPrevented()) {
+		// handle the invalid form...
+			formErrorSub();
+			submitMSGSub(false, "Please enter your email correctly.");
+		} else {
+			// everything looks good!
+			event.preventDefault();
+		}
+	});
+	function callbackFunction (resp) {
+		if (resp.result === "success") {
+			formSuccessSub();
+		}
+		else {
+			formErrorSub();
+		}
+	}
+	function formSuccessSub(){
+		$(".newsletter-form")[0].reset();
+		submitMSGSub(true, "Thank you for subscribing!");
+		setTimeout(function() {
+			$("#validator-newsletter").addClass('hide');
+		}, 4000)
+	}
+	function formErrorSub(){
+		$(".newsletter-form").addClass("animated shake");
+		setTimeout(function() {
+			$(".newsletter-form").removeClass("animated shake");
+		}, 1000)
+	}
+	function submitMSGSub(valid, msg){
+		if(valid){
+			var msgClasses = "validation-success";
+		} else {
+			var msgClasses = "validation-danger";
+		}
+		$("#validator-newsletter, #validator-newsletter-2").removeClass().addClass(msgClasses).text(msg);
+	}
+	
+	// AJAX MailChimp JS
+	$(".newsletter-form").ajaxChimp({
+		url: "https://Malice Theme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
+		callback: callbackFunction
+	});
+
+	// WOW JS
+	if($('.wow').length){
+		var wow = new WOW({
+			mobile: false
+		});
+		wow.init();
+	}
+	
+	// Odometer JS
+	$('.odometer').appear(function(e) {
+		var odo = $(".odometer");
+		odo.each(function() {
+			var countNumber = $(this).attr("data-count");
+			$(this).html(countNumber);
+		});
+	});
+
+	// Date Picker
+	$('#datetimepicker, #datetimepicker-end').datepicker({
+		weekStart: 0,
+		todayBtn: "linked",
+		language: "es",
+		orientation: "bottom auto",
+		keyboardNavigation: false,
+		autoclose: true
+	});
+	
+	// Popup Video JS
+	$('.popup-youtube, .popup-vimeo').magnificPopup({
+		disableOn: 300,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false,
+	});
+
+	// Newsletter Modal
+	$(document).ready(function(){       
+		$('#exampleModal').modal('show');
+	}); 
+
+	// Input Plus & Minus Number JS
+	$('.input-counter').each(function() {
+		var spinner = jQuery(this),
+		input = spinner.find('input[type="text"]'),
+		btnUp = spinner.find('.plus-btn'),
+		btnDown = spinner.find('.minus-btn'),
+		min = input.attr('min'),
+		max = input.attr('max');
+		
+		btnUp.on('click', function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+		btnDown.on('click', function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+	});
+
+})(jQuery);
